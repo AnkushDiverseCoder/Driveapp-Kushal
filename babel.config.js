01c5efe34415fdd47aa1ab7318de0192d5bda122
Thakur Ankush Singh <Thakurankushsingh1902@gmail.com>
2025-09-19 17:28:19 +0530

    Implement code changes to enhance functionality and improve performance

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
-  api.cache(false);
+  api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",             // Nativewind plugin
+      "react-native-reanimated/plugin" // Reanimated plugin MUST be last
     ],
-    plugins: ['react-native-worklets/plugin']
   };
 };
